17d4318bff06a6405d997b9718afc539b1ac82f5
rachelblock2 <lof16002@byui.edu>
2021-10-16 01:27:55 -0600

    Finished week5 tutorials

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,37 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('testing', 'Testing', '../../../assets/images/test.jpg'),
-        new Recipe('Another Test Recipe', 'Testing', '')
+        new Recipe('Chicken Tacos', 'The best dinner ever', '../../../assets/images/chicken_fajitas.jpg', 
+        [
+            new Ingredient('Chicken', 1),
+            new Ingredient('Peppers', 3),
+            new Ingredient('Onion', 1),
+            new Ingredient('Green onion', 1)
+        ]),
+        new Recipe('Lasagna', 'Christmas Eve dinner!', '../../../assets/images/lasagna.jpg', 
+        [
+            new Ingredient('Noodles box', 1),
+            new Ingredient('Sauce jar', 1),
+            new Ingredient('Cheese in cups', 2),
+            new Ingredient('Meat in cups', 2)
+        ])
       ];
 
+    constructor(private slService: ShoppingListService) {}
+    
     getRecipes() {
         return this.recipes.slice(); //only a copy
+    }
+
+    addIngredientstoShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
     }
 }
